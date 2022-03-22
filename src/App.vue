6e617b620a8ed41fd44952fa9d2bfdf3f9643cf8
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-03-22 23:16:25 +0600

    first simple component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Home />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Home,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Hello from home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home-Page",
+};
+</script>
