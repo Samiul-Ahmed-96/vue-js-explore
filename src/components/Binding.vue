a735205c1161235a9fc18c295ad917c9ad5aeabd
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-04-03 01:27:52 +0600

    Explore Binding

--- src/components/Binding.vue
+++ src/components/Binding.vue
@@ -11,6 +11,23 @@
   <h2>V-html</h2>
   <hr />
   <div v-html="myOfficeTwo"></div>
+  <hr />
+  <h2>V-bind</h2>
+  <hr />
+  <div v-bind:id="divId">V-bind</div>
+  <hr />
+  <h2>V-bind-attributes</h2>
+  <hr />
+  <button v-bind:disabled="isDisabled">Bind Boolean-True</button>
+  <br />
+  <br />
+  <button v-bind:disabled="isDisabled === false">Bind Boolean-False</button>
+  <hr />
+  <h2>V-bind-classes</h2>
+  <hr />
+  <div class="normal-class" v-bind:class="status">Status</div>
+  <br />
+  <div v-bind:class="isSold ? 'sold' : 'available'">Soldout Movie</div>
 </template>
 
 <script>
@@ -21,9 +38,20 @@ export default {
       name: "Hello Vue",
       myOffice: "Bangladesh Software Solution",
       myOfficeTwo: "<b>Bangladesh Software Solution</b>",
+      divId: "v-bind",
+      isDisabled: true,
+      status: "dynamic-class",
+      isSold: false,
     };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.sold {
+  color: red;
+}
+.available {
+  color: green;
+}
+</style>
